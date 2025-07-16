35405c600201bb40f8353873d112f31f749838f3
MZyoot <m.zyoot.gmail.com>
2025-07-16 19:32:19 +0300

    Fix base href and redeploy

---
+++ lib/omcoai.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OMCOAI extends StatefulWidget {
+  const OMCOAI({super.key});
+
+  @override
+  State<OMCOAI> createState() => _OMCOAIState();
+}
+
+class _OMCOAIState extends State<OMCOAI> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hi');
+  }
+}
