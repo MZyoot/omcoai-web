7c5cb65c9ff724dfcd9030ca68a8bafbdb859617
MZyoot <m.zyoot.gmail.com>
2025-07-17 23:22:01 +0300

    Fix base href and redeploy

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "0654b86217250ec2f84d6751faeb5d7e",
+"flutter_bootstrap.js": "a0f7d41cb367eb18cd3f52b9ad6d89b1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "3618b86c2bf2fffd5c6efd62c9a09471",
 "/": "3618b86c2bf2fffd5c6efd62c9a09471",
-"main.dart.js": "3c9fc2a6f0b9c9f76ab2d9c7ed231bf3",
+"main.dart.js": "ad76a52dca83009827084045d5b90404",
 "manifest.json": "18597d510eb1fe99d83fe79c6a331d0a",
 "version.json": "66573d09e4967793d82e6d232474c4bd"};
 // The application shell files that are downloaded before a service worker can
